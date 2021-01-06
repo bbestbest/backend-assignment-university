ec9f6c70a9bb4387bdd02c91a6dad0158da2064a
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-01-06 12:48:56 +0700

    add CRUD to student & university controller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -15,10 +15,34 @@ class StudentController {
         return { status: 200, error: undefined, data: data}
     }
     async store ({ request }) {
-        const { first_name, last_name } = request.body
+        const { body } = request
+        const { first_name, last_name, university_id } = body
         const data = await StudentModel.create(body)
 
         return { status: 200, error: undefined, data: data}
+    }
+    async update ({ request }) {
+        const { params, body } = request
+        const { id } = params
+        const { first_name, last_name, university_id } = body
+        let data = await StudentModel.find(id)
+        data.merge(body)
+        await data.save()
+
+        return { status: 200, error: undefined, data: data}
+    }
+    async delete ({ request }) {
+        const { params } = request
+        const { id } = params
+        let message = "";
+        let data = await StudentModel.find(id)
+        if(data !== null) {
+            await data.delete()
+            message = "Delete Success`"
+        } else {
+            message = "Delete Fail"
+        }
+        return { status: 200, error: undefined, message: message}
     }
 }
 
--- app/Controllers/Http/UniversityController.js
+++ app/Controllers/Http/UniversityController.js
@@ -5,8 +5,44 @@ const UniversityModel = use('App/Models/University')
 class UniversityController {
     async index () {
         const data = await UniversityModel.query().fetch()
-        
+
         return { status: 200, error: undefined, data: data}
+    }
+    async show ({ request }) {
+        const { id } = request.params
+        const data = await UniversityModel.find({id})
+
+        return { status: 200, error: undefined, data: data}
+    }
+    async store ({ request }) {
+        const { body } = request
+        const { short_name, full_name} = body
+        const data = await UniversityModel.create(body)
+ 
+        return { status: 200, error: undefined, data: data}
+    }
+    async update ({ request }) {
+        const { params, body } = request
+        const { id } = params
+        const { short_name, full_name } = body
+        let data = await UniversityModel.find(id)
+        data.merge(body)
+        await data.save()
+
+        return { status: 200, error: undefined, data: data}
+    }
+    async delete ({ request }) {
+        const { params } = request
+        const { id } = params
+        let message = "";
+        let data = await UniversityModel.find(id)
+        if(data !== null) {
+            await data.delete()
+            message = "Delete Success`"
+        } else {
+            message = "Delete Fail"
+        }
+        return { status: 200, error: undefined, message: message}
     }
 }
 
