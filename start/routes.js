3d4b0161b59f4377b10014d8dab19adef9ceb06b
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-01-06 10:57:58 +0700

    edit routes

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.group(() => {
+    Route.get('/student','StudentController.index')
+    Route.get('/student/:id','StudentController.show')
+    Route.post('/student','StudentController.store')
+}).prefix('api/v1')
