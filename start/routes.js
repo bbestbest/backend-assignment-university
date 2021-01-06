9256cc195dc395ab60ae1364655709a4b4c5fdfc
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-01-06 12:49:11 +0700

    add route for student & university

--- start/routes.js
+++ start/routes.js
@@ -20,4 +20,12 @@ Route.group(() => {
     Route.get('/student','StudentController.index')
     Route.get('/student/:id','StudentController.show')
     Route.post('/student','StudentController.store')
+    Route.patch('/student/:id','StudentController.update')
+    Route.delete('/student/:id','StudentController.delete')
+
+    Route.get('/university','UniversityController.index')
+    Route.get('/university/:id','UniversityController.show')
+    Route.post('/university','UniversityController.store')
+    Route.patch('/university/:id','UniveristyController.update')
+    Route.delete('/univeristy/:id','UniveristyController.delete')
 }).prefix('api/v1')
