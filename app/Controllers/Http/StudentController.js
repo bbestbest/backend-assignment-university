a28a948f641380a4f56da5780f81b548fd78fce5
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-01-07 23:12:52 +0700

    add return status for store

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -21,34 +21,32 @@ class StudentController {
         const { first_name, last_name, university_name, degree} = body
         const student_infomation = await StudentModel.query().where({ first_name, last_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
         const university_infomation = await UniversityModel.query().where({ full_name: university_name, education_degree: degree}).fetch().then(response => JSON.parse(JSON.stringify(response)))
-        for(let i = 0; i < 10; i++) {
-            if(student_infomation.length && university_infomation.length) {
-                // const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
-                const getStudentId = await StudentModel.query().where({ first_name, last_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                const getUniversityId = await UniversityModel.query().where({ full_name: university_name, education_degree: degree }).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                const checkBridge = await BridgeModel.query().where({student_id: getStudentId[0].id, university_id: getUniversityId[0].id}).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                if (!checkBridge.length) {
-                    const bridge = await BridgeModel.create({ student_id: getStudentId[0].id, university_id: getUniversityId[0].id})
-                }
-                return { status: 200, error: undefined, data: { student: student_infomation, universty_name: university_name, education_degree: degree }, message: 'Success'}
-            } else if (!student_infomation.length && university_infomation.length) {
-                const student = await StudentModel.create({ first_name, last_name })
-                // const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
-                const getStudentId = await StudentModel.query().max('id as id').then(response => JSON.parse(JSON.stringify(response)))
-                const getUniversityId = await UniversityModel.query().where({ full_name: university_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                const checkBridge = await BridgeModel.query().where({student_id: getStudentId[0].id, university_id: getUniversityId[0].id}).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                if (!checkBridge.length) {
-                    const bridge = await BridgeModel.create({ student_id: getStudentId[0].id, university_id: getUniversityId[0].id})
-                }
-                return { status: 200, error: undefined, data: { student: [student], universty_name: university_name, education_degree: degree }, message: 'Success'}
-            } else if (!university_infomation.length) {
-                const checkUniversity = await UniversityModel.query().where({ full_name: university_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
-                if(checkUniversity.length) {
-                    const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
-                    // return { status: 200, error: undefined, message: 'Create degree'}
-                } else {
-                    // return { status: 200, error: undefined}
-                }
+        if(student_infomation.length && university_infomation.length) {
+            // const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
+            const getStudentId = await StudentModel.query().where({ first_name, last_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            const getUniversityId = await UniversityModel.query().where({ full_name: university_name, education_degree: degree }).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            const checkBridge = await BridgeModel.query().where({student_id: getStudentId[0].id, university_id: getUniversityId[0].id}).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            if (!checkBridge.length) {
+                const bridge = await BridgeModel.create({ student_id: getStudentId[0].id, university_id: getUniversityId[0].id})
+            }
+            return { status: 200, error: undefined, data: { student: student_infomation, universty_name: university_name, education_degree: degree }, message: 'Success'}
+        } else if (!student_infomation.length && university_infomation.length) {
+            const student = await StudentModel.create({ first_name, last_name })
+            // const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
+            const getStudentId = await StudentModel.query().max('id as id').then(response => JSON.parse(JSON.stringify(response)))
+            const getUniversityId = await UniversityModel.query().where({ full_name: university_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            const checkBridge = await BridgeModel.query().where({student_id: getStudentId[0].id, university_id: getUniversityId[0].id}).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            if (!checkBridge.length) {
+                const bridge = await BridgeModel.create({ student_id: getStudentId[0].id, university_id: getUniversityId[0].id})
+            }
+            return { status: 200, error: undefined, data: { student: [student], universty_name: university_name, education_degree: degree }, message: 'Success'}
+        } else if (!university_infomation.length) {
+            const checkUniversity = await UniversityModel.query().where({ full_name: university_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
+            if(checkUniversity.length) {
+                const education = await UniversityModel.create({ full_name: university_name, education_degree: degree})
+                return { status: 200, error: undefined, message: 'Create degree for university'}
+            } else {
+                return { status: 200, error: undefined, message:'University not found'}
             }
         }
     }
