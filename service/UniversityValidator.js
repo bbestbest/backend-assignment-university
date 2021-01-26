fd5ab49563525c625f78ffc98a8e67fe6d6c9096
Your Name <rattaphoom_eikumnerd@hotmail.com>
2021-01-26 16:41:48 +0700

    add UniversityValidator

---
+++ service/UniversityValidator.js
@@ -0,0 +1,28 @@
+const Validator = use("Validator");
+
+module.exports = async function UniversityValidator(data) {
+  if (typeof data !== "object") throw new Error();
+
+  const { email, username, password, display_name } = data;
+
+  const rules = {
+    username: "required",
+    password: "required|min:8",
+    email: "required|email|unique:users,email",
+    display_name: "required|min:5",
+  };
+
+  const validation = await Validator.validateAll(
+    {
+      email,
+      username,
+      password,
+      display_name,
+    },
+    rules
+  );
+
+  return {
+    error: validation.messages(),
+  };
+};
